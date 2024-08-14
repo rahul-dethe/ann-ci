ed5b7e724ead11d7bfb256b5c717f016dbf20f91
Rahul Dethe <81143446+rahul-dethe@users.noreply.github.com>
2024-08-14 18:00:48 +0530

    Add files via upload

---
+++ ann-ci_v1/packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+conda install pytorch -y
+conda install numba -y
+conda install scikit-learn -y
+conda install pandas -y
+conda install matplotlib -y
+conda install numpy -y
+conda install -c conda-forge lapack -y
+pip install bitstring
+
